70f13a197229f257f6b466c1b0682400054f3537
awirth <wirth.a@huskey.neu.edu>
2020-11-22 11:14:44 -0800

    Migrated Sound Logic not quite working

--- brain/brain.cc
+++ brain/brain.cc
@@ -1,26 +1,34 @@
 
 #include <iostream>
 #include <math.h>
+#include <string>
 #include <unistd.h>
 
 #include "robot.hh"
 
 using std::cout;
 using std::endl;
+using namespace std;
 
+int maxsound = 0;
+bool turning = false ;
+string lastdir = "forward";
+int lastsound = 0;
+float pos_t;
 int stateCount;
+
 void
 callback(Robot* robot)
 {
-    cout << robot->get_line_status() << endl;
+    /*cout << robot->get_line_status() << endl;
 
     cout << robot->get_noise_sensor() << endl;
 
     cout << robot->get_robot_theta() << endl;
-
+*/
     // robot->set_vel(-5.0, 5.0)
 
-    stateCount++;
+    /*stateCount++;
     std::cout << "state count: " << stateCount << '\n';
     if(stateCount < 3){
          robot->set_vel(1.5, 1.5);
@@ -29,8 +37,68 @@ callback(Robot* robot)
     } else if(stateCount > 6 ){
       stateCount = 0;
       robot->set_vel(-1.5, 1.5);
-    }
+    }*/
+cout << robot->get_noise_sensor() <<" "  << robot->get_robot_theta() << " "<< lastdir <<  endl;
+pos_t = robot->get_robot_theta();
+  
 
+int currentsound = 0;
+
+
+if (currentsound >= maxsound) {
+        robot->set_vel(4, 4);
+	maxsound= currentsound;
+}
+
+else if (currentsound < maxsound  && lastdir == "forward" && lastsound != currentsound  ) {
+	turning =  true ;
+	if(abs(pos_t) > 2.5){
+	robot->set_vel(4, 4);
+	lastdir = "backward";
+	lastsound= currentsound;	
+	}
+			else{
+	robot->set_vel(-2, 2);}
+
+	}
+else if(currentsound <= maxsound && lastdir == "backward" &&  lastsound != currentsound ) {
+         if(pos_t > 1.25 && pos_t < 1.75) {
+          robot->set_vel(4, 4);
+          lastdir = "left";           
+          lastsound= currentsound;
+
+	 }
+                          else{
+          robot->set_vel(-2, 2);}
+          
+  
+	}
+  else if(currentsound <= maxsound && lastdir == "left" && lastsound != currentsound) {
+           if(pos_t < -1.25 && pos_t > -1.75) {
+            robot->set_vel(4, 4);
+            lastdir = "right";          
+            lastsound= currentsound;
+ 
+	   }
+                            else{
+            robot->set_vel(-2, 2);}                
+          }
+else if (currentsound < maxsound  && lastdir == "right" && lastsound != currentsound ) {
+        turning =  true ;
+        if(abs(pos_t) < 1){
+        robot->set_vel(4, 4);
+        lastdir = "backward";
+        lastsound= currentsound;
+
+	}
+                        else{
+        robot->set_vel(-2, 2);}
+
+        }
+else {
+        robot->set_vel(4, 4);
+        
+}
     return;
 }
 
